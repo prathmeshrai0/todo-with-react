3ae26999e6703973c5f0cb49d6d64ea2257a2a9f
prathmeshrai0 <jatinrai862000@gmail.com>
2025-03-21 12:54:20 +0530

    icon is removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Icon from './components/icon'
 import Navbar from './components/Navbar'
 import tick from './assets/tick.svg'
 import { FaEdit } from "react-icons/fa";
